cdbddd5d21e512c54d3af70926306ef22878cca9
Alicia Mauldin <alicia.a.mauldin@gmail.com>
2018-05-23 23:27:08 -0700

    Updated navbars to align formatting on the Kiosk link.

---
+++ PPAttendanceSystem/PPAttendanceSystem/Controllers/LandingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5051.Controllers
+{
+    public class LandingController : Controller
+    {
+        // GET: Landing
+        public ActionResult Landing()
+        {
+            return View();
+        }
+    }
+}
