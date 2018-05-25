0193ffc2ff8ddb6cb5b1f0cd6606405b7d632cf1
Chidozie Ononiwu <ononiwuchidozie@live.com>
2018-05-24 22:03:55 -0700

    Added custom JavaScript Code to StudentDashboard Page

---
+++ PPAttendanceSystem/PPAttendanceSystem/Scripts/studentDashboard.js
@@ -0,0 +1,27 @@
+﻿// JavaScript Code for the StudentDashboard page
+window.onload = function () {
+    var studentCards = document.getElementsByClassName('check-in');
+    for (let i = 0; i < studentCards.length; i++) {
+        var studentCard = studentCards[i];
+        studentCard.onclick = function () {
+            let sID = "stu" + (i + 1);
+            let stuId = document.getElementById(sID);
+            $(".student-card-info > img").remove();
+            $(".student-card-info span").remove();
+            $(".student-card-info").prepend(stuId.innerHTML);
+            $(".check-in-popup").addClass("active");
+        }
+    }
+}
+
+
+function checkedIn() {
+    var checkInTime = new Date().toLocaleTimeString();
+    alert("Thank you for checking in today. Check in time: " + checkInTime);
+    $(".student-card-info > img").remove();
+    $(".student-card-info span").remove();
+    $(".check-in-popup").removeClass("active");
+}
+
+
+
