14340f02dbada069a0656036345f13ee01522da7
Alicia Mauldin <alicia.a.mauldin@gmail.com>
2018-05-30 00:16:23 -0700

    Added in temp d3.js graph to custom metrics

---
+++ PPAttendanceSystem/PPAttendanceSystem/Scripts/prettyGraphs-d3.js
@@ -0,0 +1,15 @@
+﻿/***************************
+ * D3.js customized charts *
+ * *************************/
+
+// Hard-coded student data (hours per day in a week)
+var data = [30, 86, 168, 281, 303, 365];
+
+// Simple bar chart
+d3.select(".chart")
+	.selectAll("div")
+	.data(data)
+	.enter()
+	.append("div")
+	.style("width", function (d) { return d + "px"; })
+	.text(function (d) { return d; });
