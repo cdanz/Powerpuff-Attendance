fef0e41eb40a6de66e29077d03afe4b5d4d4ee50
Chidozie Ononiwu <ononiwuchidozie@live.com>
2018-05-31 21:28:09 -0700

    Chages to Layout of Kiosk Page

--- PPAttendanceSystem/PPAttendanceSystem/Scripts/studentDashboard.js
+++ PPAttendanceSystem/PPAttendanceSystem/Scripts/studentDashboard.js
@@ -1,16 +1,15 @@
 ﻿// JavaScript Code for the StudentDashboard page
+
 window.onload = function () {
     var studentCards = document.getElementsByClassName('check-in');
+    
     for (let i = 0; i < studentCards.length; i++) {
         var studentCard = studentCards[i];
         studentCard.onclick = function () {
             let sID = "stu" + (i + 1);
             let stuId = document.getElementById(sID);
-            $(".student-card-info > img").remove();
-            $(".student-card-info span").remove();
-            $(".student-card-info").prepend(stuId.innerHTML);
-            $(".check-in-box").css('background-color', $(stuId).css('background-color'));
-            $(".check-in-popup").addClass("active");
+            let classtoHide = ".emoticon" + (i + 1)
+            $(classtoHide).toggleClass("active");
         }
     }
 }
@@ -19,9 +18,6 @@ window.onload = function () {
 function checkedIn() {
     var checkInTime = new Date().toLocaleTimeString();
     alert("Thank you for checking in today. Check in time: " + checkInTime);
-    $(".student-card-info > img").remove();
-    $(".student-card-info span").remove();
-    $(".check-in-popup").removeClass("active");
 }
 
 
