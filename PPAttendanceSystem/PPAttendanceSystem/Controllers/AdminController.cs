6a5c5dbcc0ca278e8d88f6b471c888992c0fc8d2
Alicia Mauldin <alicia.a.mauldin@gmail.com>
2018-06-03 00:37:14 -0700

    Fixed table formatting; updated vars in table JS file; adding in roster of 9 students next.

--- PPAttendanceSystem/PPAttendanceSystem/Controllers/AdminController.cs
+++ PPAttendanceSystem/PPAttendanceSystem/Controllers/AdminController.cs
@@ -15,54 +15,6 @@ namespace _5051.Controllers
 
             return View();
         }
-        public ActionResult AdminDashboardwkm1()
-        {
-            ViewBag.Message = "Admin Dashboard1";
-
-            return View();
-        }
-        public ActionResult AdminDashboardwkm2()
-        {
-            ViewBag.Message = "Admin Dashboard2";
-
-            return View();
-        }
-        public ActionResult AdminDashboardwkm3()
-        {
-            ViewBag.Message = "Admin Dashboard3";
-
-            return View();
-        }
-        public ActionResult AdminDashboardmon()
-        {
-            ViewBag.Message = "Admin Dashboardm";
-
-            return View();
-        }
-        public ActionResult AdminDashboardtues()
-        {
-            ViewBag.Message = "Admin Dashboardt";
-
-            return View();
-        }
-        public ActionResult AdminDashboardwed()
-        {
-            ViewBag.Message = "Admin Dashboardw";
-
-            return View();
-        }
-        public ActionResult AdminDashboardthurs()
-        {
-            ViewBag.Message = "Admin Dashboardth";
-
-            return View();
-        }
-        public ActionResult AdminDashboardfri()
-        {
-            ViewBag.Message = "Admin Dashboardf";
-
-            return View();
-        }
 
         // GET: Class Roster
         public ActionResult ClassRoster()
--- PPAttendanceSystem/PPAttendanceSystem/Scripts/tableContent.js
+++ PPAttendanceSystem/PPAttendanceSystem/Scripts/tableContent.js
@@ -1,31 +1,31 @@
 ﻿function edit_row(no) {
-    document.getElementById("edit_button" + no).style.display = "none";
-    document.getElementById("save_button" + no).style.display = "block";
+	document.getElementById("edit_button" + no).style.display = "section-scroll btn btn-border-w btn-round";
+	document.getElementById("save_button" + no).style.display = "section-scroll btn btn-border-w btn-round";
 
-    var name = document.getElementById("name_row" + no);
-    var country = document.getElementById("country_row" + no);
-    var age = document.getElementById("age_row" + no);
+    var fname = document.getElementById("fname_row" + no);
+    var lname = document.getElementById("lname_row" + no);
+    var status = document.getElementById("status_row" + no);
 
-    var name_data = name.innerHTML;
-    var country_data = country.innerHTML;
-    var age_data = age.innerHTML;
+    var fname_data = fname.innerHTML;
+    var lname_data = lname.innerHTML;
+    var status_data = status.innerHTML;
 
-    name.innerHTML = "<input type='text' id='name_text" + no + "' value='" + name_data + "'>";
-    country.innerHTML = "<input type='text' id='country_text" + no + "' value='" + country_data + "'>";
-    age.innerHTML = "<input type='text' id='age_text" + no + "' value='" + age_data + "'>";
+    fname.innerHTML = "<input type='text' class='form-control' id='fname_text" + no + "' value='" + fname_data + "'>";
+	lname.innerHTML = "<input type='text' class='form-control' id='lname_text" + no + "' value='" + lname_data + "'>";
+	status.innerHTML = "<input type='text' class='form-control' id='status_text" + no + "' value='" + status_data + "'>";
 }
 
 function save_row(no) {
-    var name_val = document.getElementById("name_text" + no).value;
-    var country_val = document.getElementById("country_text" + no).value;
-    var age_val = document.getElementById("age_text" + no).value;
+    var fname_val = document.getElementById("fname_text" + no).value;
+    var lname_val = document.getElementById("lname_text" + no).value;
+    var status_val = document.getElementById("status_text" + no).value;
 
-    document.getElementById("name_row" + no).innerHTML = name_val;
-    document.getElementById("country_row" + no).innerHTML = country_val;
-    document.getElementById("age_row" + no).innerHTML = age_val;
+    document.getElementById("fname_row" + no).innerHTML = fname_val;
+    document.getElementById("lname_row" + no).innerHTML = lname_val;
+    document.getElementById("status_row" + no).innerHTML = status_val;
 
-    document.getElementById("edit_button" + no).style.display = "block";
-    document.getElementById("save_button" + no).style.display = "none";
+	document.getElementById("edit_button" + no).style.display = "section-scroll btn btn-border-w btn-round";
+	document.getElementById("save_button" + no).style.display = "section-scroll btn btn-border-w btn-round";
 }
 
 function delete_row(no) {
@@ -33,15 +33,15 @@ function delete_row(no) {
 }
 
 function add_row() {
-    var new_name = document.getElementById("new_name").value;
-    var new_country = document.getElementById("new_country").value;
-    var new_age = document.getElementById("new_age").value;
+    var new_fname = document.getElementById("new_fname").value;
+    var new_lname = document.getElementById("new_lname").value;
+    var new_status = document.getElementById("new_status").value;
 
     var table = document.getElementById("data_table");
     var table_len = (table.rows.length) - 1;
-    var row = table.insertRow(table_len).outerHTML = "<tr id='row" + table_len + "'><td id='name_row" + table_len + "'>" + new_name + "</td><td id='country_row" + table_len + "'>" + new_country + "</td><td id='age_row" + table_len + "'>" + new_age + "</td><td><input type='button' id='edit_button" + table_len + "' value='Edit' class='edit' onclick='edit_row(" + table_len + ")'> <input type='button' id='save_button" + table_len + "' value='Save' class='save' onclick='save_row(" + table_len + ")'> <input type='button' value='Delete' class='delete' onclick='delete_row(" + table_len + ")'></td></tr>";
+	var row = table.insertRow(table_len).outerHTML = "<tr id='row" + table_len + "'><td id='fname_row" + table_len + "'>" + new_fname + "</td><td id='lname_row" + table_len + "'>" + new_lname + "</td><td id='status_row" + table_len + "'>" + new_status + "</td><td><input type='button' id='edit_button" + table_len + "' value='Edit' class='section-scroll btn btn-border-w btn-round' onclick='edit_row(" + table_len + ")'> <input type='button' id='save_button" + table_len + "' value='Save' class='section-scroll btn btn-border-w btn-round' onclick='save_row(" + table_len + ")'> <input type='button' value='Delete' class='section-scroll btn btn-border-w btn-round' onclick='delete_row(" + table_len + ")'></td></tr>";
 
-    document.getElementById("new_name").value = "";
-    document.getElementById("new_country").value = "";
-    document.getElementById("new_age").value = "";
+    document.getElementById("new_fname").value = "";
+    document.getElementById("new_lname").value = "";
+	document.getElementById("new_status").value = "";
 }
