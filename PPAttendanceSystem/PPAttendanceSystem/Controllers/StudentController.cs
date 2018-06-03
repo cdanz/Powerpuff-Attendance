add3b205418f153bdac8481a7333f5aae88735ec
Alicia Mauldin <alicia.a.mauldin@gmail.com>
2018-06-03 12:42:25 -0700

    Restored access to dummy student pages (Home, Report, fake Edit Profile)

--- PPAttendanceSystem/PPAttendanceSystem/Controllers/StudentController.cs
+++ PPAttendanceSystem/PPAttendanceSystem/Controllers/StudentController.cs
@@ -6,6 +6,30 @@ namespace _5051.Controllers
 {
     public class StudentController : Controller
     {
+        // GET: Student profile edit page
+        public ActionResult StudentHome()
+        {
+            ViewBag.Message = "Student Home";
+
+            return View();
+        }
+
+        // GET: Student profile edit page
+        public ActionResult StudentReport()
+        {
+            ViewBag.Message = "Student Report";
+
+            return View();
+        }
+
+        // GET: Student profile edit page
+        public ActionResult StudentEditProfile()
+        {
+            ViewBag.Message = "Student Edit Profile";
+
+            return View();
+        }
+
         // A ViewModel used for the Student that contains the StudentList
         private StudentViewModel StudentViewModel = new StudentViewModel();
 
@@ -230,12 +254,6 @@ namespace _5051.Controllers
             StudentBackend.Delete(data.Id);
 
             return RedirectToAction("Index");
-        }
-        public ActionResult StudentReport()
-        {
-            ViewBag.Message = "Student Report";
-
-            return View();
         }
     }
 }
