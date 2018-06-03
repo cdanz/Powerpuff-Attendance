0f4c79cad0dd97bfabbb48c6c8bc5f084474a503
Alicia Mauldin <alicia.a.mauldin@gmail.com>
2018-06-03 02:00:04 -0700

    Updated settings page

---
+++ PPAttendanceSystem/PPAttendanceSystem/Scripts/time.js
@@ -0,0 +1,18 @@
+﻿var input = $('#input-a');
+input.clockpicker({
+	autoclose: true
+});
+
+// Manual operations
+$('#button-a').click(function (e) {
+	// Have to stop propagation here
+	e.stopPropagation();
+	input.clockpicker('show')
+		.clockpicker('toggleView', 'minutes');
+});
+$('#button-b').click(function (e) {
+	// Have to stop propagation here
+	e.stopPropagation();
+	input.clockpicker('show')
+		.clockpicker('toggleView', 'hours');
+});
