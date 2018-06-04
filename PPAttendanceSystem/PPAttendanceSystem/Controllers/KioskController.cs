b4f8db9252d9ddf8a8633e295988f0fddca2e120
Alicia Mauldin <9796054+mauldina@users.noreply.github.com>
2018-06-04 14:31:31 -0700

    Added comments to kiosk functions

--- PPAttendanceSystem/PPAttendanceSystem/Controllers/KioskController.cs
+++ PPAttendanceSystem/PPAttendanceSystem/Controllers/KioskController.cs
@@ -7,6 +7,7 @@ namespace _5051.Controllers
     public class KioskController : Controller
     {
 
+        // GET: Old, faked view of student kiosk page
         public ActionResult StudentDashboard()
         {
             ViewBag.Message = "Student Dashboard";
@@ -14,6 +15,7 @@ namespace _5051.Controllers
             return View();
         }
 
+        // GET: Old, faked view of add new student page
         public ActionResult StudentNew()
         {
             ViewBag.Message = "Student New";
